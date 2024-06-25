bdee143d99ff528e3517707479636efe800150ce
Eden <eden@bit.dev>
2024-06-25 21:33:40 +0300

    ...

--- storefront/storefront/storefront.tsx
+++ storefront/storefront/storefront.tsx
@@ -1,13 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import { ProductGalleryPage } from '@bit-bazaar/storefront.pages.product-gallery-page';
-import { Layout } from './layout.js';
 
 export function Storefront() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="/products" element={<ProductGalleryPage />} />
-      </Route>
+      <Route path="/" element={<h1>Store</h1>} />
+      <Route path="/products" element={<ProductGalleryPage />} />
     </Routes>
   );
 }
